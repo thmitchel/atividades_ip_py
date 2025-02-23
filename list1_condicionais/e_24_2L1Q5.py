8165985aec89b37df7b3bc37f7ce19648ba7a175
thmitchel <thiagomsilva@fac.pe.senac.br>
2025-02-23 01:18:38 -0300

    Adicionando código da atividade 6

--- list1_condicionais/e_24_2L1Q5.py
+++ list1_condicionais/e_24_2L1Q5.py
@@ -1,7 +1,7 @@
 orcamento_total = float(input("Digite o valor total em reaisi para a festa do Rodri: "))
 numero_convidados = int(input("Digite a quantidade de convidados que estarão presentes na festa: "))
 local_festa = input("Local onde a festa será realizada: ").lower()
-cantor_convidado = input("Digite o cantor contratado para a festa: ").lower()
+cantor_convidado = input("Digite o cantor contratado para a festa: ").lower
 cotacao_euro = float(input("Digite a cotação do euro: "))
 orcamento_euro = orcamento_total / cotacao_euro
 gasto_convidado = orcamento_euro / numero_convidados
@@ -28,4 +28,4 @@ if(cantor_convidado == "anitta" or "thiaguinho"):
     print(f"Duvido que aceite, Rodri fez isso pra me estressar! Claro que {cantor_convidado} não vai fazer isso comigo!")
 
 else:
-    print("Vou fazer uma oferta maior! Isso precisa flopar!")
+    print("Vou fazer uma oferta maior! Isso precisa flopar!")   
--- list1_condicionais/f_24_2L1Q6.py
+++ list1_condicionais/f_24_2L1Q6.py
@@ -4,6 +4,35 @@ taxa_variavel_empresa_1 = float(input("Digite a taxa variável da empresa 1: "))
 taxa_fixa_empresa_2 = float(input("Digite a taxa fixa da empresa 2: "))
 taxa_variavel_empresa_2 = float(input("Digite a taxa variável da empresa 2: "))
 
-k = float(input("Digite a distancia em quilometros: "))
-P1 = (taxa_variavel_empresa_1 * k) + taxa_fixa_empresa_1
-P2 = (taxa_variavel_empresa_2 * k) + taxa_fixa_empresa_2
+#k = float(input("Digite a distancia em quilometros: "))
+#P1 = (taxa_variavel_empresa_1 * k) + taxa_fixa_empresa_1
+#P2 = (taxa_variavel_empresa_2 * k) + taxa_fixa_empresa_2
+
+if(taxa_fixa_empresa_1 == taxa_fixa_empresa_2 and taxa_variavel_empresa_1 == taxa_variavel_empresa_2):
+    print("As duas empresas tenhem o mesmo preço sempre!")
+
+elif(taxa_variavel_empresa_1 == taxa_variavel_empresa_2):
+    if(taxa_fixa_empresa_1 < taxa_fixa_empresa_2):
+        print("A empresa 1 tem o melhor preço")
+    else:
+        print("A empresa 2 tem o melhor preço!")
+
+elif(taxa_fixa_empresa_1 == taxa_fixa_empresa_2):
+    if(taxa_variavel_empresa_1 < taxa_variavel_empresa_2):
+        print("A empresa 1 tem o melhor preço!")
+    else:
+        print("A empresa 2 tem o melhor preço!")
+
+else:
+    k = (taxa_fixa_empresa_2 - taxa_fixa_empresa_1) / (taxa_variavel_empresa_1 - taxa_variavel_empresa_2)
+
+    if k > 0:
+        if taxa_variavel_empresa_1 < taxa_variavel_empresa_2:
+            print(f"Empresa 1 é mais barata para distâncias menores que {k:.2f} km, ambas têm o mesmo preço para {k:.2f} km e a Empresa 2 é mais barata para distâncias maiores que {k:.2f} km.")
+        else:
+            print(f"Empresa 2 é mais barata para distâncias menores que {k:.2f} km, ambas têm o mesmo preço para {k:.2f} km e a Empresa 1 é mais barata para distâncias maiores que {k:.2f} km.")
+    else:
+        if taxa_fixa_empresa_1 < taxa_fixa_empresa_2:
+            print("A Empresa 1 é sempre a melhor opção!")
+        else:
+            print("A Empresa 2 é sempre a melhor opção!")
