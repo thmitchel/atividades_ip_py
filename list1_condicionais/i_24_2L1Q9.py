7a7e3b4530917918a3bd1ad23e95d63710b2bb54
THIAGO MITCHEL DA SILVA <86864044+thmitchel@users.noreply.github.com>
2025-03-14 17:03:32 -0300

    Update i_24_2L1Q9.py

---
+++ list1_condicionais/i_24_2L1Q9.py
@@ -0,0 +1,14 @@
+nome_suspeito1 = input("Digite o nome do primeiro suspeito: ")
+distancia_suspeito1 = int(input("Digite a distância do primeiro suspeito à Bola de Ouro: "))
+
+nome_suspeito2 = input("Digite o noem do segundo suspeito: ")
+distancia_suspeito2 = int(input("Digite a distância do segundo suspeito à Bola de Ouro: "))
+
+
+categoria_suspeito1 = input("Digite a categoria do primeiro suspeito: ")
+categoria_suspeito2 = input("Digite a categoria do segundo suspeito: ")
+
+if(distancia_suspeito1 % 2 == 0 and distancia_suspeito2 % 2 == 0):
+    print("{nome_suspeito1} e {nome_suspeito2} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!")
+
+elif: 
